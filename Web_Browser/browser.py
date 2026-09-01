5e10fecf1466e4bc866c1a0bfe6c449ad4855636
Shineast <canfrog@naver.com>
2026-09-01 17:00:46 +0900

    [Web Browser] HTTP 요청 전송 및 응답(헤더/본문) 파싱 구현 |

--- Web_Browser/browser.py
+++ Web_Browser/browser.py
@@ -20,3 +20,31 @@ class URL:
         )
         # 80번 포트에 연결
         s.connect((self.host, 80))
+
+        # format 메서드를 통해 path와 host 바인딩
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n" # 두 번의 줄바꿈을 통해 서버에 요청 전송
+        s.send(request.encode("utf9")) # encode()는 텍스트를 바이트로 변환함
+
+        # 바이트가 포함된 파일 형식의 객체를 반환하며 루프를 감추는 헬퍼 함수 makefile 사용
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n": break # 빈줄을 만나면 헤더 파싱 루프 종료
+            header, value = line.split(":", 1) # 콜론(:)을 기준으로 헤더 이름과 헤더 값 분리
+            response_headers[header.casefold()] = value.strip() # 소문자 표준화 및 공백, 줄바꿈 문자 제거
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        # 헤더 이후 남은 데이터 스트림을 모두 읽어오고 소켓 연결 해제
+        body = response.read()
+        s.close()
+
+        # 수신된 최종 문자열 반환
+        return body
